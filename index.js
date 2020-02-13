22a57a9b2ce55a0dcc6104d434db4217a317985c
Robert Elias <RobertElias@users.noreply.github.com>
2020-02-13 10:33:37 -0800

    updates to data

--- index.js
+++ index.js
@@ -1,18 +1,29 @@
-const express = require('express');
+const express = require("express");
 
 const server = express();
 
 server.use(express.json());
 
-server.get('/', (reg, res)=>{
-    const songs = [{
-        id: 1, name: "Final Countdown"
-    }]
-    res.status(200).json(songs);
-})
+server.get("/", (reg, res) => {
+  const songs = [
+    {
+      id: 1,
+      name: "Final Countdown"
+    },
+    {
+        id: 2,
+        name: "Welcome To The Jungle"
+      },
+      {
+        id: 3,
+        name: "We are the Champions"
+      }
+  ];
+  res.status(200).json(songs);
+});
 
 //make the port dynamic
 const port = process.env.PORT || 5000;
 server.listen(port, () => {
-    console.log(`\n*** Server Running on ${port} ***\n`);
-  }); 
+  console.log(`\n*** Server Running on ${port} ***\n`);
+});
