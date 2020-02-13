50ffc47e70e0756032099dd8b1e847b5e6e09981
Robert Elias <RobertElias@users.noreply.github.com>
2020-02-13 10:29:53 -0800

    Makes port dynamic

--- index.js
+++ index.js
@@ -11,7 +11,8 @@ server.get('/', (reg, res)=>{
     res.status(200).json(songs);
 })
 
-const port = 5000;
+//make the port dynamic
+const port = process.env.PORT || 5000;
 server.listen(port, () => {
     console.log(`\n*** Server Running on ${port} ***\n`);
-  });
+  }); 
